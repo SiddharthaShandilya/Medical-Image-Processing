9a77be592d82e686b4590d8165d93c591861b1a6
siddharthashandilya <siddharthashandilya104@gmail.com>
2022-09-21 11:42:23 +0530

    Put my name and email id in it

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name="src",
     version="0.0.1",
-    author="c17hawke",
-    description="A small package for dvc dl pipeline demo",
+    author="SIddhartha",
+    description="A small package for healthcare dl pipeline demo",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/c17hawke/DVC_DL_Tensorflow_demo",
-    author_email="sunny.c17hawke@gmail.com",
+    url="https://github.com/SiddharthaShandilya/Medical-Image-Processing.git",
+    author_email="siddharthashandilya104@gmail.com",
     packages=["src"],
     python_requires=">=3.7",
     install_requires=[
